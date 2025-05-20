0f03f6ff328a97490e33254d8e06c4b59b36a1c7
Gaurav Patil <gauravp8991@gmail.com>
2025-05-20 16:45:21 +0530

    Range Addition using Difference array technique

---
+++ RangeAddition.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class RangeAddition {
+    public static int[] modifiedArray(int length, int updates[][]) {
+
+        int result[] = new int[length + 1];
+        for(int i[] : updates){
+        int start = i[0];
+        int end = i[1];
+        int inc= i[2];
+
+        result[start] += inc;
+        if(end + 1 < length){
+        result[end + 1] += inc;
+        }
+        }
+
+        for(int i = 1; i <length; i++){
+        result[i] += result[i-1];
+        }
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+        int length = 5;
+        int updates[][] = { { 1, 3, 2 }, { 2, 4, 3 }, { 0, 2, -2 } };
+        System.out.println(Arrays.toString(modifiedArray(length, updates)));
+    }
+}
