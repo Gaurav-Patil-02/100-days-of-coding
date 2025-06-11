d00111f24d03a4ca5c2578fbc549f3957ae691ea
Gaurav Patil <gauravp8991@gmail.com>
2025-06-11 08:27:50 +0530

    GCD using Euclidean Algo

---
+++ GCD.java
@@ -0,0 +1,20 @@
+public class GCD {
+    public static int gcd(int a, int b){
+        while(a > 0 && b > 0){
+            if(a > b){
+                a = a % b;
+            }else{
+                b = b % a;
+            }
+        }
+
+        if(a == 0){
+            return b;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(gcd(10, 15));
+    }
+}
