a089f47f59cc76246f53ba3642c46e62755ae963
Gaurav Patil <gauravp8991@gmail.com>
2025-06-11 09:13:03 +0530

    Check if number is prime

---
+++ PrimeNumbers.java
@@ -0,0 +1,18 @@
+public class PrimeNumbers {
+    public static boolean isPrime(int n){
+        if(n <= 1) return false;
+       if(n ==  2) return true;
+       
+
+        for(int i = 2; i <= n/2; i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPrime(17));
+    }
+}
