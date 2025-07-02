2f5cceecd94dd6e723339674447d75ede2e5b74c
Gaurav Patil <gauravp8991@gmail.com>
2025-07-02 07:57:40 +0530

    Find the Original Typed String II

---
+++ FindTheOriginalTypedString_II.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FindTheOriginalTypedString_II {
+     public static int possibleStringCount(String word, int k) {
+    List<Integer> groups = getConsecutiveLetters(word);
+    final int totalCombinations =
+        (int) groups.stream().mapToLong(Integer::longValue).reduce(1L, (a, b) -> a * b % MOD);
+    if (k <= groups.size())
+      return totalCombinations;
+
+    
+    int[] dp = new int[k];
+    dp[0] = 1;
+
+    for (int i = 0; i < groups.size(); ++i) {
+      int[] newDp = new int[k];
+      int windowSum = 0;
+      int group = groups.get(i);
+      for (int j = i; j < k; ++j) {
+        newDp[j] = (newDp[j] + windowSum) % MOD;
+        windowSum = (windowSum + dp[j]) % MOD;
+        if (j >= group)
+          windowSum = (windowSum - dp[j - group] + MOD) % MOD;
+      }
+      dp = newDp;
+    }
+
+    final int invalidCombinations = Arrays.stream(dp).reduce(0, (a, b) -> (a + b) % MOD);
+    return (totalCombinations - invalidCombinations + MOD) % MOD;
+  }
+
+  private static final int MOD = 1_000_000_007;
+  private static List<Integer> getConsecutiveLetters(final String word) {
+    List<Integer> groups = new ArrayList<>();
+    int group = 1;
+    for (int i = 1; i < word.length(); ++i)
+      if (word.charAt(i) == word.charAt(i - 1)) {
+        ++group;
+      } else {
+        groups.add(group);
+        group = 1;
+      }
+    groups.add(group);
+    return groups;
+  }
+    public static void main(String[] args) {
+       String word = "aabbccdd";
+       int  k = 8;
+       System.out.println(possibleStringCount(word, k));
+    }
+}
--- MergesSort.java
+++ MergesSort.java
@@ -4,11 +4,16 @@ import java.util.Arrays;
 public class MergesSort {
 
     public static void merge(int arr[], int low, int mid, int high){
+
+        //Temp array to store sorted part
         ArrayList<Integer> temp = new ArrayList<>();
+
         int left = low;
         int right = mid + 1;
 
+        //Run till left and right are less than mid and high
         while (left <= mid && right <= high) {
+            //Sort the elements by switching the elements
             if(arr[left] <= arr[right]){
                 temp.add(arr[left]);
                 left++;
@@ -17,7 +22,7 @@ public class MergesSort {
                 right++;
             }
         }
-
+        //Sort remaining elements
         while (left <= mid) {
             temp.add(arr[left]);
             left++;
@@ -27,18 +32,30 @@ public class MergesSort {
             right++;
         }
 
+        //Copy the temp elements to original array
         for(int i = low; i <= high; i++){
             arr[i] = temp.get(i - low);
         }
     }
 
     public static void mergeSort(int arr[], int low, int high){
+        //Retunr if base case hits
         if(low >= high) return;
+
+        //Find mid
         int mid = (low + high) / 2;
+
+        //Divide first half
         mergeSort(arr, low, mid);
+
+        //Divide Seconde half
         mergeSort(arr, mid+1, high);
+
+        //sort and merge
         merge(arr, low, mid, high);
     }
+
+
     public static void main(String[] args) {
         int arr[] = { 9, 4, 7, 6, 3, 1, 5 };
         mergeSort(arr, 0, arr.length - 1);
